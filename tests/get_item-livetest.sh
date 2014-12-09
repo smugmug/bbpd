6c55c0b3a4ce8814f456826fac6baef0e6a53f8e
brad clawsie <brad@smugmug.com>
2014-12-09 13:29:40 -0800

    update tests

--- tests/get_item-livetest.sh
+++ tests/get_item-livetest.sh
@@ -1,4 +1,3 @@
-curl -H "X-Bbpd-Indent: true" -H "X-Amz-Target: DynamoDB_20120810.GetItem" -X POST -d '{"TableName":"test-godynamo-livetest","Key":{"TheHashKey":{"S":"a-hash-key-json1"},"TheRangeKey":{"N":"1"}}}' "http://localhost:12333/?compact=1&indent=1";
+curl -H "X-Bbpd-Indent: true" -H "X-Amz-Target: DynamoDB_20120810.GetItem" -X POST -d '{"TableName":"test-godynamo-livetest","Key":{"TheHashKey":{"S":"a-hash-key-json1"},"TheRangeKey":{"N":"1"}}}' "http://localhost:12333/";
 echo "";
-curl -H "X-Bbpd-Verbose: True" -H "X=BBPD-Verbose: True" -X POST -d '{"TableName":"test-godynamo-livetest","Key":{"TheHashKey":{"S":"a-hash-key-json1"},"TheRangeKey":{"N":"1"}}}' "http://localhost:12333/GetItemJSON?compact=1";
-
+curl -H "X-Bbpd-Verbose: True" -X POST -d '{"TableName":"test-godynamo-livetest","Key":{"TheHashKey":{"S":"a-hash-key-json1"},"TheRangeKey":{"N":"1"}}}' "http://localhost:12333/GetItemJSON";
